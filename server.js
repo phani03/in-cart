c2300927d0d363b70a4789234a7c20eb4c6cb6d1
Phaneendra Varma Kattari <Kattari_Phaneendra_V@cat.com>
2019-03-28 13:03:23 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8008);
-
+app.use(express.static(__dirname + '/dist/in-cart'));
 app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/in-cart/index.html'));
 })
-
+app.listen(process.env.PORT || 8008);
 console.log('started listening.')
